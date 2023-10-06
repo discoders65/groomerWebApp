1f0ae19259c7b5b648f82fcc2ba543dcf1e69732
w-jadczak <w.jadczak@yahoo.com>
2023-10-06 15:09:17 +0200

    Initial commit

---
+++ src/main/java/com/wjadczak/groomerWebApp/GroomerWebApplication.java
@@ -0,0 +1,13 @@
+package com.wjadczak.groomerWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroomerWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wjadczak/groomerWebApp/GroomerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjadczak.groomerWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
