d15b5ca2e4f43751f23c8f533a65e7e6df1b7b73
w-jadczak <w.jadczak@yahoo.com>
2024-04-08 20:29:18 +0200

    adding eureka client & updating spring version to 3.2.4

--- src/main/java/com/wjadczak/groomerWebApp/GroomerWebApplication.java
+++ src/main/java/com/wjadczak/groomerWebApp/GroomerWebApplication.java
@@ -3,11 +3,13 @@ package com.wjadczak.groomerWebApp;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableEncryptableProperties
 @EnableTransactionManagement
+@EnableDiscoveryClient
 public class GroomerWebApplication {
 
 	public static void main(String[] args) {
